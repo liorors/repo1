c6a7be9feef1b90ac5224a6d62d3228b6e195cbf
Liorors <liorors>
2023-07-22 13:24:25 +0300

    Lior2

--- Lior.java
+++ Lior.java
@@ -1,7 +1,7 @@
 public class Lior{
 public static void main(String[] args) {
-for (int i=1;i<=10; i++) {
-System.out.println("My name is Lior ..."+i);
+for (int i=1;i<=12; i++) {
+System.out.println("My name is Lior Or ..."+i);
 }
 }
 }
